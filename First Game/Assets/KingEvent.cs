35508c8f3c700214451af3e7e2511468b27e43f3
Gaven <Gaven.le0921@gmail.com>
2019-01-30 11:41:37 -0700

    Updated Unity Project

---
+++ First Game/Assets/KingEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KingEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+    
+    private void OnCollisionEnter2D()
+    {
+        Event.Invoke();
+    }
+}
