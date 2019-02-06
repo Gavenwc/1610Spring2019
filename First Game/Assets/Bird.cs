38cff0bb39a6d77a6568cde2e76cb809678774f6
Gaven <Gaven.le0921@gmail.com>
2019-02-06 11:20:26 -0700

    Updated First Game

--- First Game/Assets/Bird.cs
+++ First Game/Assets/Bird.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Bird : Animal
 {
+    public Color FeatherColor;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<SpriteRenderer>().color = SkinColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,10,0);
     }
 }
