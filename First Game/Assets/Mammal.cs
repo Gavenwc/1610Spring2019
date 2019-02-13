b7943b1e65f6b6a48df3bfc5c15702a1204e3f57
Gaven <Gaven.le0921@gmail.com>
2019-02-12 21:56:43 -0700

    Updated First Game

--- First Game/Assets/Mammal.cs
+++ First Game/Assets/Mammal.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Mammal : Animal
 {
     public Color FurColor;
+
+    public int FurCount = 10000;
+    public float EatingSpeed = 0.4f;
+    public bool CanEat = true;
+    public UnityEvent Event;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+     Event.Invoke();   
     }
 
     // Update is called once per frame
