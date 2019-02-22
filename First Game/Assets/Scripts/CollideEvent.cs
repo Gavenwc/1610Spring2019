10b1bb26caa854e397b9f6cf5c23a22b7d5fccc3
Gaven <Gaven.le0921@gmail.com>
2019-02-21 23:52:33 -0700

    Updated First Game

---
+++ First Game/Assets/Scripts/CollideEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollideEvent : MonoEvents
+{
+    public UnityEvent Event;
+
+    private void OnCollison3D()
+    {
+        Event.Invoke();
+    }
+
+}
