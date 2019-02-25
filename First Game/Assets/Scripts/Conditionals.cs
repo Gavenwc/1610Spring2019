9b818e8c7a8b1bec75644d90b6f9bf6d85122619
Gaven <Gaven.le0921@gmail.com>
2019-02-25 11:10:18 -0700

    Updated First Game

---
+++ First Game/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+    public bool CanWalk = true;
+    public int Number;
+    public string Password;
+    void Start()
+    {
+        
+    }
+
+   private void Update()
+    {
+        if (CanWalk)
+        {
+            print("True");
+        }
+        else
+        {
+            print(false);
+        }
+
+        if (Number >= 10)
+        {
+            print("Greater");
+        }
+        else
+        {
+            print("Lesser");
+        }
+
+        if (Password == "YaBoi")
+        {
+            print("...acceptable...");
+        }
+        else
+        {
+            print("Hell Nah");
+        }
+    }
+}
